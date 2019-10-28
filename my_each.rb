cb85cd3e961c2c22f0257492695cda2cde2bc48d
Ricky dildy <rdildy92@gmail.com>
2019-10-28 00:27:19 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,3 +3,10 @@ collection = [1, 2, 3, 4]
 def my_each(collection) do |i|
   puts i 
 end
+
+def yielding 
+  puts my_each(collection)
+  while my_each do |collection|
+    collection + 1 
+  end 
+end 
